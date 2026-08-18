ad26ae0114543d2bf33c109cf00b3c12f4b82ae2
Prerna Gaur <prernagaur7677@gmail.com>
2026-08-18 20:21:47 +0530

    Add files via upload
    Basics

---
+++ Day1.java
@@ -0,0 +1,150 @@
+import java.util.Scanner;
+
+public class Day1{
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+        // System.out.println("Hey, Prerna.");
+        // System.out.println("Printed.");
+        // int a = 10;
+        // int b = 5;
+        // System.out.println(a+b);
+        // System.out.println(a-b);
+        // System.out.println(a*b);
+        // System.out.println(a/b);
+        // System.out.println(a%b);
+        // System.out.println(a<b);
+        // System.out.println(a<=b);
+        // System.out.println(a>b);
+        // System.out.println(a>+b);
+        // System.out.println(a==b);
+        // System.out.println(a!=b);
+
+
+
+        // Practice Question
+        // Question_1
+        // System.out.print("Enter the first number: ");
+        // int a = sc.nextInt();
+        // System.out.print("Enter the second number: ");
+        // int b = sc.nextInt();
+        // int sum = a+b;
+        // System.out.println("Sum : " + sum);
+
+
+
+        // Question_2
+        // System.out.print("Enter the principal: ");
+        // int p = sc.nextInt();
+        // System.out.print("Enter the rate of interest: ");
+        // int r = sc.nextInt();
+        // System.out.print("Enter the time period: ");
+        // int t = sc.nextInt();
+        // double si = (p*r*t)/100.0;
+        // System.out.println("Simple Interest : " + si);
+
+
+
+        // Question_3
+        // System.out.print("Enter age: ");
+        // int age = sc.nextInt();
+        // if(age >= 18){
+        //     System.out.println("Can vote");
+        // }else{
+        //     System.out.println("Cannot vote");
+        // }
+
+
+
+        // Question_4
+        // System.out.print("Enter number: ");
+        // int num = sc.nextInt();
+        // if(num%2 == 0){
+        //     System.out.println("Even number.");
+        // }else{
+        //     System.out.println("Even number.");
+        // }
+
+
+
+        // Question_5
+        // System.out.print("Enter the first number: ");
+        // int num1 = sc.nextInt();
+        // System.out.print("Enter the second number: ");
+        // int num2 = sc.nextInt();
+        // System.out.print("Enter the second number: ");
+        // int num3 = sc.nextInt();
+        // if(num1>=num2  && num1>=num3){
+        //     System.out.println("num1 is greater than num2 and num3.");
+        // }else if(num2>=num3){
+        //     System.out.println("num2 is greater than num1 and num3.");
+        // }else{
+        //     System.out.println("num3 is greater than num1 and num2.");
+        // }
+
+
+
+        // Question_6
+        // System.out.print("Enter the number: ");
+        // int marks = sc.nextInt();
+        // if(marks>=75){
+        //     System.out.println("Grade A.");
+        // }else if(marks<75  && marks>=65){
+        //     System.out.println("Grade B.");
+        // }else if(marks<65  && marks>=55){
+        //     System.out.println("Grade C.");
+        // }else if(marks<55  && marks>=45){
+        //     System.out.println("Grade D.");
+        // }else{
+        //     System.out.println("Fail.");
+        // }
+
+
+
+        // Question_7
+        // System.out.print("Enter the number: ");
+        // int nums = sc.nextInt();
+        // int i = 1;
+        // while(i<=nums){
+        //     System.out.println(i + " : Prerna");
+        //     i++;
+        // }
+
+
+    
+        // Question_8
+        // System.out.print("Enter the number: ");
+        // int nums = sc.nextInt();
+        // int i = 1;
+        // while(i<=nums){
+        //     System.out.print(i + "   ");
+        //     i++;
+        // }
+
+
+
+        // Question_9
+        // System.out.print("Enter the number till you want sum of: ");
+        // int nums = sc.nextInt();
+        // int i = 1, sum = 0;
+        // while(i<=nums){
+        //     sum += i;
+        //     i++;
+        // }
+        // System.out.print("Total : " + sum);
+
+
+
+        // Question_10
+        // System.out.print("Enter the number: ");
+        // int nums = sc.nextInt();
+        // while(nums != 0){
+        //    int rem = nums % 10;
+        //    if(rem%2 == 0){
+        //     System.out.println(rem);
+        //    }
+        //    nums /= 10;
+        // }
+
+        sc.close();
+    }
+}
